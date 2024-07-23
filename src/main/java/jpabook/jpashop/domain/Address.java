b170b26389d109e4b7058efde59a90d77df5dd7e
sesung <sesungg@gmail.com>
2024-07-23 23:39:27 +0900

    first commit

---
+++ src/main/java/jpabook/jpashop/domain/Address.java
@@ -0,0 +1,18 @@
+package jpabook.jpashop.domain;
+
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Embeddable
+@Getter
+@AllArgsConstructor
+public class Address {
+
+    private String city;
+    private String street;
+    private String zipcode;
+
+    protected Address() {
+    }
+}
---
+++ src/test/java/jpabook/example/jpabook2/Jpabook2ApplicationTests.java
@@ -0,0 +1,13 @@
+package jpabook.example.jpabook2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jpabook2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
